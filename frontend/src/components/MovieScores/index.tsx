d5bb2c32c6ed03bad2cab3d4416c94eceb234cd3
Rodrigo P. Capucho <xpiolx@hotmail.com>
2022-03-10 01:31:36 -0300

    MovieCard

---
+++ frontend/src/components/MovieScores/index.tsx
@@ -0,0 +1,19 @@
+
+import MovieStarts from 'components/MovieStars';
+import './styles.css'
+
+function MovieScores() {
+
+    const score = 2.5;
+    const count = 12;
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStarts />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
+
+export default MovieScores;
